35a0c987e34764e85f3c6415a083a2024901cbb1
mmarakesh <marinamakarova749@gmail.com>
2023-09-12 23:02:40 +0200

    create hello

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import villa from './villa.png';
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <h1>Test</h1>
       <p>Hello, it is me!!!</p>
-=======
-      <img src={villa} alt="villa" />
-      <p>Villas</p>
->>>>>>> 764cfb4f28799825a41871d9819fa3f70ad9ec4d
+      <p>!!!!git</p>
     </div>
   );
 }
