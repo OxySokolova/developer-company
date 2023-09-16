2569d524a12ac592edfb7feffc4af8028d9988be
mmarakesh <marinamakarova749@gmail.com>
2023-09-16 11:27:26 +0200

    add component Process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import About from './About';
 import './App.css';
+import Process from './Process';
 function App() {
   return (
     <div>
       <About/>
+      <Process/>
     </div>
   );
 }
---
+++ src/Process.js
@@ -0,0 +1,9 @@
+function Process() {
+    return(
+        <div>
+            Test
+        </div>
+    )
+}
+
+export default Process;
