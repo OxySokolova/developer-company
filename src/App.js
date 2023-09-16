0a577dc3660d9c1bdbb7d6ccbc3e9dbef860c8e6
mmarakesh <marinamakarova749@gmail.com>
2023-09-17 00:12:11 +0200

    delete component Process.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import About from './About';
 import './App.css';
-import Process from './Process';
 import Video from './Video';
 function App() {
   return (
     <div>
       <Video />
       <About/>
-      <Process/>
     </div>
   );
 }
