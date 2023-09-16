eb357d63af6c77886de2c55bb0e73d7a522c82de
mmarakesh <marinamakarova749@gmail.com>
2023-09-16 10:55:56 +0200

    delete villa.png and september13

--- src/About.js
+++ src/About.js
@@ -1,5 +1,5 @@
 import Slides from "./Slides";
-
+import './App.css';
 
 function About() {
     return(
--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import About from './About';
 import './App.css';
-import villa from './villa.png';
 function App() {
   return (
     <div>
-      <About/>
-      <p>September 13</p>
-      <img src={villa} alt="villa"/>
-
+    <About/>
     </div>
   );
 }
