4b753aab3f97d1d78ce8cc5e5ddbbd6c9e0c07ac
OxySokolova <poksanaa16@gmail.com>
2023-09-16 15:25:59 -0500

    text above the video

--- src/Video.js
+++ src/Video.js
@@ -6,10 +6,12 @@ class Video extends Component{
 
     render(){
     return(
-        <div>
-        <video autoPlay muted loop id="myVideo" playsinline>
-            <source src={video} type="video/mp4"/>
-        </video>
+        <div className="video">
+            <p className="above">WE WILL BUILD YOUR DREAM VILLA</p>
+            <video autoPlay muted loop id="myVideo" playsinline>
+                <source src={video} type="video/mp4"/>
+            </video>
+            
         </div>
     )
     }
