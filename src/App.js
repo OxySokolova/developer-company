036473e66a787580c65bc8c2719aa5129cf1449a
OxySokolova <poksanaa16@gmail.com>
2023-09-15 20:49:16 -0500

    logo

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import villa from './villa.png';
 function App() {
   return (
     <div>
-      <p>September 13</p>
-      <p>Hello, it is me!!!</p>
+      <img src={villa} alt="villa" width="50px" />
+      
     </div>
   );
 }
