b84347fd56bf7c73392b2d02b5024a62d318c061
mmarakesh <marinamakarova749@gmail.com>
2023-10-02 13:56:54 +0200

    add new component Price.js

--- src/Price.js
+++ src/Price.js
@@ -1,7 +1,17 @@
 function Price() {
     return(
-        <div>
-    
+        <div className="priceBox">
+    <div className="priceItem">
+        <h2 className="priceNumber">149 000$</h2>
+        <p className="priceText">without rice fields view</p>
+    </div>
+    <div className="priceItem">
+        <img src="https://static.tildacdn.com/tild3130-3131-4232-b762-346664303064/free-icon-dollar-sym.png" width="80px"/>
+    </div>
+    <div className="priceItem">
+        <h2 className="priceNumber">169 000$</h2>
+        <p className="priceText">with rice fields view</p>
+    </div>
         </div>
     )
 }
