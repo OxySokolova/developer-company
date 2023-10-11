f1d51e8394bfd7853d46beb99fc86804d2666838
OxySokolova <poksanaa16@gmail.com>
2023-10-10 22:32:41 -0500

    react-router-hash-link

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
+import React from 'react';
 import About from './About';
 import './App.css';
 import Video from './Video';
-import Menu from './Menu';
 import Price from './Price';
 import Feedback from './Feedback';
 import Information from './Information';
-import End from './End'
+import End from './End';
+import logo from './villa-modified.png';
+import { HashLink } from 'react-router-hash-link';
+
 
 function App() {
   return (
     <div>
-      <Menu />
+      <nav className="nav">
+        <HashLink  to="/#about" className='item'>ABOUT US</HashLink>
+        <HashLink to="/#projects" className=' item'>PROJECTS</HashLink>
+        <img src={logo} alt="logo" width="60px"/>
+        <HashLink  to="/#process" className=' item'>PROCESS</HashLink>
+        <HashLink  to="/#consultation" className=' item'>CONSULTATION</HashLink>
+      </nav>
+    <div>
       <Video />
-      <About/>
-      <Price/>
-      <Feedback />
-      <Information/>
+
+      <section id="about"><About /></section>
+
+      <section id="projects"><Price /></section>
+
+      <section id="consultation"><Feedback /></section>
+
+      <section id="process"><Information /></section>
+
       <End />
+    </div>
     </div>
   );
 }
