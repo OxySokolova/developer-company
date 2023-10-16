6dd161763870b28d5e184fea63727dc422a6dbdf
mmarakesh <marinamakarova749@gmail.com>
2023-10-16 19:49:28 +0200

    mobile adaptation for iPad

--- src/Slides.js
+++ src/Slides.js
@@ -25,7 +25,7 @@ function Slides() {
         })
     }
     return(
-        <div className="slidesContainer">  
+        <div className="slidesContainer"> 
         <div className="slidesBtnAndImage">
         <button className="btnSlides" onClick={previousBtn}> 
         <svg className="slidesSvg" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/> </svg>
@@ -50,7 +50,8 @@ function Slides() {
         <p className="parText"><span className="blueWords">GUEST</span> TOILET</p>
         <hr/>
         <p className="parText"><span className="blueWords">12 М²</span> POOL</p>
-    </div>
+    </div> 
+
     <div className="investContainerSlides">
         <h1 className="blueWords parTextHead">THE RETURN ON INVESTMENT IS 5-6 YEARS</h1>
         <h2 className="parTextHead">THAILAND COMPANY AND
